261f4ab60cbbbd19664f3287a18b3c86028bd198
VeganCzech <63636174+VeganCzech@users.noreply.github.com>
2021-01-25 15:56:31 +0100

    Add project files.

---
+++ WebAPI/Models/Users.cs
@@ -0,0 +1,20 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace WebAPI.Models
+{
+    public class Users
+    {
+        public int Id { get; set; }
+        public string FirstName { get; set; }
+        public string LastName { get; set; }
+        public string Gender { get; set; }
+        public string Password { get; set; }
+        public string PasswordSeed { get; set; }
+        public DateTime DateOfBirth { get; set; }
+        public DateTime LastActive { get; set; }
+
+    }
+}
